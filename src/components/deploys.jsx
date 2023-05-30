24fa8f2f001502661df7754edc252089af0d4536
vimayan <joblessdesks@gmail.com>
2023-05-30 23:21:15 +0530

    sites update

--- src/components/deploys.jsx
+++ src/components/deploys.jsx
@@ -1,9 +1,57 @@
 import React from "react";
-import Ticket from "../data/Ticket.jpeg";
+import Ticket from "../data/Ticket.jpg";
 import VegSite from "../data/vegsite.jpeg";
 import ClonedApp from "../data/ClonedApp.jpeg";
+import Fitness from "../data/fitness-logger.jpeg";
+import Shortner from "../data/shortner.png";
+import SpaceX from "../data/spacex.png";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 function Deploys() {
+  const sites = [
+    {
+      img: Fitness,
+      front: "https://github.com/vimayan/Fitness_Logger_frontend",
+      back: "https://github.com/vimayan/guvi-hackathon2-backend",
+      launch: "https://fitness-loggerl-villavan.netlify.app",
+      size: { color: "black", marginX: "3px" },
+    },
+    {
+      img: Ticket,
+      front: "https://github.com/vimayan/guvi-hackathon2-frontend",
+      back: "https://github.com/vimayan/guvi-hackathon2-backend",
+      launch: "https://guvi-hackathon2-frontend-villavan.netlify.app",
+      size: { color: "black", marginX: "3px" },
+    },
+    {
+      img: Shortner,
+      front:
+        "https://github.com/vimayan/GUVI-Tasks/tree/main/url-shortner/client",
+      back: "https://github.com/vimayan/GUVI-Tasks/tree/main/url-shortner/server",
+      launch: "https://tinyshortner.netlify.app",
+      size: { color: "black", marginX: "3px" },
+    },
+    {
+      img: SpaceX,
+      front: "https://github.com/vimayan/spacex-clone",
+      launch: "https://spacex-lazylode.netlify.app",
+      size: { color: "#e3f2fd", marginX: "3px" },
+    },
+    {
+      img: VegSite,
+      front:
+        "https://github.com/vimayan/GUVI-Tasks/tree/main/react-tasks/task-3",
+      launch: "https://villavan-react-task3.netlify.app",
+      size: { color: "black", marginX: "3px" },
+    },
+    {
+      img: ClonedApp,
+      front: "https://github.com/vimayan/job-clone",
+      launch: "https://villavan-react-training.netlify.app",
+      size: { color: "lime", marginX: "3px" },
+    },
+  ];
   return (
     <>
       <div className="container my-4" id="deploys">
@@ -13,86 +61,50 @@ function Deploys() {
             <p className="fs-6">Something I have built and deployed</p>
           </div>
         </div>
-        
-        <div className="card-group">
 
-          <div className="card ticket">
-            <a href="https://guvi-hackathon2-frontend-villavan.netlify.app/">
-              <img src={Ticket} className="card-img-top"  alt="..." />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Ticket Booking</h5>
-              <p className="card-text">
-                <b  onClick={()=>window.open("https://github.com/vimayan/guvi-hackathon2-frontend")} className="me-1 text-primary">Front end</b>
-                <span>ReactJS React-router-Dom Redux Formik Yup Bootstrap</span>
-                <br />
-                <b onClick={()=>window.open("https://github.com/vimayan/guvi-hackathon2-backend")} className="me-2 text-primary">Back end</b>
-                <span>
-                  NodeJS Express jwt joi bcryptjs mongoose SocketIO{" "}
-                </span>{" "}
-              </p>
+        <div className="row">
+          {sites.map((site, index) => (
+            <div key={index} className="col-12 col-md-4 p-0 sites">
+              <img src={site.img} className="img-fluid h-100" alt="..." />
+              <div className="icons">
+                <p className="text">
+                  <b className="me-2 badge bg-primary">Front end</b>
+                  <GitHubIcon
+                    fontSize="large"
+                    sx={site.size}
+                    onClick={() =>
+                      window.open(
+                        "https://github.com/vimayan/guvi-hackathon2-frontend"
+                      )
+                    }
+                  />
+                  <br />
+                  {site.back ? (
+                    <>
+                      <b className="me-2 badge bg-primary">Back end</b>
+                      <GitHubIcon
+                        fontSize="large"
+                        sx={site.size}
+                        onClick={() =>
+                          window.open(
+                            "https://github.com/vimayan/guvi-hackathon2-backend"
+                          )
+                        }
+                      />
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </p>
+                <p className="text">
+                  <b className="me-2 badge bg-primary">Launch site</b>
+                  <a href={site.launch}>
+                    <LaunchIcon fontSize="large" sx={site.size} />
+                  </a>
+                </p>
+              </div>
             </div>
-            <div className="card-footer bg-info">
-              <small className="text-muted">
-                <a
-                  href="https://github.com/vimayan/guvi-hackathon2-frontend"
-                  className="text-decoration-none text-black card-text"
-                >
-                  https://github.com/vimayan/guvi-hackathon2-frontend
-                </a>
-              </small>
-            </div>
-          </div>
-
-
-
-          <div className="card">
-            <a href="https://villavan-react-task3.netlify.app/">
-              <img src={VegSite} className="card-img-top" alt="..." />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Veg Shop</h5>
-              <p className="card-text">
-                <b onClick={()=>window.open("https://github.com/vimayan/GUVI-Tasks/tree/main/react-tasks/task-3")} className="me-1 text-primary">Front end</b>
-                <span>ReactJS Bootstrap responsive cart design</span>
-              </p>
-            </div>
-            <div className="card-footer bg-info">
-              <small className="text-muted">
-                <a
-                  href="https://github.com/vimayan/GUVI-Tasks/tree/main/react-tasks/task-3"
-                  className="text-decoration-none text-black card-text"
-                >
-                  https://github.com/vimayan/GUVI-Tasks/tree/main/react-tasks/task-3
-                </a>
-              </small>
-            </div>
-          </div>
-
-
-
-          <div className="card">
-            <a href="https://villavan-react-training.netlify.app/">
-              <img src={ClonedApp} className="card-img-top" alt="..." />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Cloned</h5>
-              <p className="card-text text-start">
-                this is my first front end website before joining GUVI using
-                React JS
-              </p>
-            </div>
-            <div className="card-footer bg-info">
-              <small className="text-muted">
-                <a
-                  href="https://github.com/vimayan/job-clone"
-                  className="text-decoration-none text-black card-text"
-                >
-                  https://github.com/vimayan/job-clone
-                </a>
-              </small>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
